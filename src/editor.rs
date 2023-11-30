4683f846c0b4a6fcf1eb7657d3f7607d647471c6
nFeus <barca105@nate.com>
2023-11-30 16:51:36 +0900

    add json based method

--- src/editor.rs
+++ src/editor.rs
@@ -1,27 +1,48 @@
-use axum::async_trait;
-use serde_derive::Deserialize;
+use axum::{
+    async_trait, 
+    Json,
+    http::StatusCode
+};
+use serde::{Serialize, Deserialize};
+use tracing::Subscriber;
 
+use tracing_serde::AsSerde;
 
 #[derive(Deserialize)]
-pub struct Blog {
-    pub author: &'static str,
-    pub title: &'static str,
+pub struct WriteBlog {
+    pub author: String,
+    pub title: String,
+    pub content: String,
     pub author_id: u8,
 }
 
 
+
+#[derive(Serialize, Debug)]
+pub struct ReadBlog {
+    pub author: String,
+    pub title: String,
+    pub content: String,
+}
+
+
+
+
 #[async_trait]
 pub trait AsyncBlog {
-    async fn get_foo() -> Blog;
+    async fn post_foo(&self, Json(payload) : Json<WriteBlog>) -> (StatusCode, Json<ReadBlog>);
+
 }
 
 #[async_trait]
-impl AsyncBlog for Blog {
-    async fn get_foo() -> Blog {
-        Blog {
-            author: "Jung",
-            title: "이 것이 시작이다.",
-            author_id: 1,
-        }
+impl AsyncBlog for WriteBlog {
+    async fn post_foo(&self, Json(payload) : Json<WriteBlog>) -> (StatusCode, Json<ReadBlog>) {
+        let poster = ReadBlog {
+            author: payload.author,
+            title: payload.title,
+            content: payload.content,
+        };
+
+        (StatusCode::CREATED, Json(poster))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,85 @@
 mod editor;
 
+use tracing::info;
+
 use axum::{
-    routing::get,
+    routing::{get, post},
     Router,
+    Json,
+    http::StatusCode
 };
+
+use editor::{WriteBlog, ReadBlog};
+// use backend_rs::editor::{WriteBlog};
 
 
 #[tokio::main]
 async fn main() {
+
+
+    // you must use "dotenv".
+
+    tracing_subscriber::fmt::init();
+
+    let test_log = 3;
+
+    info!(test_log, "ok! ok!");
+
+
+
     // build our application with a single route
     let app = Router::new()
         .route("/", get(root))
         .route("/foo", get(get_foo).post(post_foo))
         .route("/foo/bar", get(foo_bar));
 
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    // route_layer, with_state, layer, nest, nest_service in Router
+    // what were these?
+
+
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
 
 async fn root() -> &'static str {
+    info!("root");
+
     "root"
+
 }
 
 
-async fn get_foo() -> &'static str {
-    "get_foo"
+async fn post_foo(Json(payload): Json<WriteBlog>) -> (StatusCode, Json<ReadBlog>) {
+    
+    let poster = ReadBlog {
+        author: payload.author,
+        title: payload.title,
+        content: payload.content,
+    };
+    info!("Logging JSON: {:#?}", poster);
+
+    (StatusCode::CREATED, Json(poster))
+
 }
 
 
-async fn post_foo() -> &'static str {
-    "post_foo"
+
+async fn get_foo() -> (StatusCode, Json<ReadBlog>) {
+    let poster = ReadBlog {
+        author: String::from("정선교"),
+        title: String::from("제목"),
+        content: String::from("본문"),
+    };
+
+    info!("Logging JSON: {:#?}", poster);
+
+
+
+    (StatusCode::CREATED, Json(poster))
 }
 
 async fn foo_bar() -> &'static str {
+    info!("foo_bar");
     "foo_bar"
 }
